ac192b2f36e6f57db7c7bbe5a80bb3665e50d142
Narshil <narshil94@gmail.com>
2022-07-23 15:18:50 +0100

    Initial commit

---
+++ imu_demo.ino
@@ -0,0 +1,19 @@
+
+#include <M5Stack.h>
+
+
+void setup() {
+    M5.begin();
+    M5.Power.begin();
+    M5.Lcd.fillScreen(WHITE);
+    delay(1000);
+}
+
+void loop() {
+    M5.Lcd.fillTriangle(random(M5.Lcd.width() - 1), random(M5.Lcd.height() - 1),
+                        random(M5.Lcd.width() - 1), random(M5.Lcd.height() - 1),
+                        random(M5.Lcd.width() - 1), random(M5.Lcd.height() - 1),
+                        random(0xfffe));
+
+    M5.update();
+}
