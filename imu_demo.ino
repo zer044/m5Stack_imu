8f1feecd2db616aa5a615b89264121dd31e994be
Narshil <narshil94@gmail.com>
2022-07-23 16:21:58 +0100

    added imu to lcd

--- imu_demo.ino
+++ imu_demo.ino
@@ -1,20 +1,34 @@
 
 #include <M5Stack.h>
 #include "SensorFusion.h"
+#include "Imu.h"
+#include <string>
 
+auto imu = Imu();
 
 void setup() {
+    Serial.begin(115200);
+    imu.begin();
+
     M5.begin();
     M5.Power.begin();
     M5.Lcd.fillScreen(WHITE);
+    M5.Imu.begin();
     delay(1000);
 }
 
 void loop() {
-    M5.Lcd.fillTriangle(random(M5.Lcd.width() - 1), random(M5.Lcd.height() - 1),
-                        random(M5.Lcd.width() - 1), random(M5.Lcd.height() - 1),
-                        random(M5.Lcd.width() - 1), random(M5.Lcd.height() - 1),
-                        random(0xfffe));
+    imu.update();
 
+    auto roll = imu.getRoll();
+    auto pitch = imu.getPitch();
+    auto yaw = imu.getYaw();
+    auto temperature = imu.getTemperature();
+
+    std::string eulers = "Roll: " + std::to_string(roll) + " Pitch: " + std::to_string(pitch) + " Yaw: " + std::to_string(yaw);
+
+    M5.Lcd.setTextColor(BLACK);
+    M5.Lcd.setCursor(0, 10);
+    M5.Lcd.printf(eulers.c_str(), 10, 10);
     M5.update();
 }
